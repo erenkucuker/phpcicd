be40b4fb0bd624a62356bed03c9a698041e3c1f0
Your Name <you@example.com>
2021-10-02 11:37:12 +0300

    fix issues30

--- deploy.php
+++ deploy.php
@@ -55,8 +55,6 @@ task('deploy', [
   'artisan:storage:link',
   'artisan:view:cache',
   'artisan:config:cache',
-  'artisan:migrate',
-  'artisan:queue:restart',
   'deploy:symlink',
   'deploy:unlock',
   'cleanup',
