7e791c9f4a343c1b8c3d839efe721763afeb2283
Your Name <you@example.com>
2021-10-02 09:39:05 +0300

    fix issues18

--- deploy.php
+++ deploy.php
@@ -30,7 +30,7 @@ task('deploy:secrets', function () {
   upload('.env', get('deploy_path') . '/shared');
 });
 
-host('myapp.io')
+host('18.195.115.161')
   ->hostname('18.195.115.161')
   ->stage('production')
   ->user('root')
